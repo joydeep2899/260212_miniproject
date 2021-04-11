6d8c2e9fde8f61583c81fe84630b2a370cc02ade
joydeep2899 <joydeep779@gmail.com>
2021-04-12 04:00:24 +0530

    beautifying code

--- 3_Implementation/src/a.c
+++ 3_Implementation/src/a.c
@@ -3,41 +3,39 @@
 
 typedef struct heapnode{
 
-char c;
+ char c;
 
-int freq;
+ int freq;
 
 
-struct heapnode *left, *right;
+ struct heapnode *left, *right;
 
 
 
 } heapnode;
 
+
+
 typedef struct Minheap{
 
+   int size,max_size;
 
 
-int size;
-
-int max_size;
-
-heapnode** array;
-
-
-
-
+   heapnode** array;
 
 } Minheap;
+
+
+
 
 
 heapnode* createnode(char ch, int frequency ){
 
 
-heapnode *temp=(heapnode *)malloc(sizeof(heapnode));
-temp->c=ch;
-temp->freq=frequency;
-temp->left=temp->right=NULL;
+    heapnode *temp=(heapnode *)malloc(sizeof(heapnode));
+    temp->c=ch;
+    temp->freq=frequency;
+    temp->left=temp->right=NULL;
 
 return temp;
 
@@ -46,12 +44,11 @@ return temp;
 
 Minheap* createminheap(int max_sizeofheap){
 
-Minheap *minheap=(Minheap*)malloc(sizeof(Minheap));
-
-
-minheap->size=0;
-minheap->max_size=max_sizeofheap;
-minheap->array=(heapnode**)malloc(max_sizeofheap*sizeof(heapnode*));
+   Minheap *minheap=(Minheap*)malloc(sizeof(Minheap));
+   
+    minheap->size=0; 
+    minheap->max_size=max_sizeofheap;
+    minheap->array=(heapnode**)malloc(max_sizeofheap*sizeof(heapnode*));
 
 return minheap;
 
@@ -61,9 +58,9 @@ return minheap;
 
 
 void heapnode_swap(heapnode **a,heapnode **b){
-heapnode* t=*a;
-*a=*b;
-*b=t;
+   heapnode* t=*a;
+   *a=*b;
+   *b=t;
 
 }
 
@@ -72,76 +69,72 @@ heapnode* t=*a;
 
 void heapify(Minheap *minheap,int index){
 
-int smallest_idx=index;
+ int smallest_idx=index;
 
-int left=2*index+1;
-int right=2*index+2;
-
-
-if(left<minheap->size&& minheap->array[left]->freq<minheap->array[smallest_idx]->freq){
-
-smallest_idx=left;
+ int left=2*index+1;
+ int right=2*index+2;
 
 
-}
-if(right<minheap->size && minheap->array[right]->freq<minheap->array[smallest_idx]->freq){
+ if(left<minheap->size&& minheap->array[left]->freq<minheap->array[smallest_idx]->freq){
 
-smallest_idx=right;
-
+  smallest_idx=left;
 
 
-}
+ }
+ if(right<minheap->size && minheap->array[right]->freq<minheap->array[smallest_idx]->freq){
+
+  smallest_idx=right;
 
 
 
-if(smallest_idx!=index){
+ }
+
+
+
+ if(smallest_idx!=index){
 
     heapnode_swap(&minheap->array[smallest_idx],&minheap->array[index]);
 
     heapify(minheap,smallest_idx);
-}
+ }
 
 
 
 }
 
-
-int checksizeone(Minheap *minheap)
-{
-
-    return (minheap->size == 1);
-}
 
 //extract minimum node from minheap
 heapnode* popminnode(Minheap *minheap){
 
-heapnode *temp=minheap->array[0];
+  heapnode *temp=minheap->array[0];
 
 
-minheap->array[0]=minheap->array[minheap->size-1];
---minheap->size;
+  minheap->array[0]=minheap->array[minheap->size-1];
+  --minheap->size;
 
-heapify(minheap,0);
+  heapify(minheap,0);
 
 
-return temp;
+  return temp;
 }
 
 
-void insertminheap(Minheap *minheap,heapnode *minheapnode){
-minheap->size++;
-int j=minheap->size-1;
+void insertminheap(Minheap *minheap,heapnode *minheapnode)
+{
+ minheap->size++;
+ int j=minheap->size-1;
 
 
-while(j && minheapnode->freq < minheap->array[(j-1)/2]->freq ){
+ while(j && minheapnode->freq < minheap->array[(j-1)/2]->freq )
+   {
 
- minheap->array[j]=minheap->array[(j-1)/2];
- j=(j-1)/2;
+    minheap->array[j]=minheap->array[(j-1)/2];
+    j=(j-1)/2;
 
 
-}
+   }
 
-minheap->array[j]=minheapnode;
+ minheap->array[j]=minheapnode;
 
 
 
@@ -164,10 +157,11 @@ void buildMinHeap(Minheap* minHeap)
 void printarray(int arr[],int n)
 {
  int i=0;
- 
+
  for(i=0; i<n;i++ ){
 
      printf("%d",arr[i]);
+     
  }
  printf("\n");
 
@@ -187,12 +181,12 @@ int checkleafnode(heapnode* minheapnode){
 Minheap* createandbuildminheap(char c[],int freq[] , int size) 
 {
 
-int i=0;
+ int i=0;
 
-Minheap *minheap= createminheap(size);
+ Minheap *minheap= createminheap(size);
 
-for(i=0;i<size;i++){
-minheap->array[i]=createnode(c[i],freq[i]);
+ for(i=0;i<size;i++){
+ minheap->array[i]=createnode(c[i],freq[i]);
 
 
 }
@@ -220,20 +214,20 @@ return minheap;
 
 heapnode* huffmantree(char c[],int freq[],int size ){
 
-Minheap* minheap=createandbuildminheap(c,freq,size);
+  Minheap* minheap=createandbuildminheap(c,freq,size);
 
-heapnode *top, *left, *right ;
+  heapnode *top, *left, *right ;
 
-while(!(minheap->size==1)){
+  while(!(minheap->size==1)){
  
- left=popminnode(minheap);
- right=popminnode(minheap);
+    left=popminnode(minheap);
+    right=popminnode(minheap);
 
- top=createnode('$',left->freq+right->freq);
- top->left=left;
- top->right=right;
+    top=createnode('$',left->freq+right->freq);
+    top->left=left;
+    top->right=right;
 
- insertminheap(minheap,top);
+    insertminheap(minheap,top);
 
 
 
@@ -247,31 +241,31 @@ return popminnode(minheap);
 
 void printhuffmancodes(heapnode* root,int ans[],int index){
 
-if(root->left){
-ans[index]=0;
-printhuffmancodes(root->left,ans,index+1);
+  if(root->left)
+   {
+    ans[index]=0;
+    printhuffmancodes(root->left,ans,index+1);
 
 
 
-}
+    }
 
-if(root->right){
-ans[index]=1;
-printhuffmancodes(root->right,ans,index+1);
+ if(root->right)
+  {
+   ans[index]=1;
+   printhuffmancodes(root->right,ans,index+1);
 
+  } 
 
-
-}
-
-if(checkleafnode(root)){
-
-    printf("%c",root->c);
+ if(checkleafnode(root))
+  {
+    printf("%c: ",root->c);
 
     printarray(ans,index);
+  
 
 
-
-}
+  }
 
 
 }
@@ -287,6 +281,9 @@ void huffmancodes(char c[],int freq[], int size){
     int ans[100],index=0;
 
     printhuffmancodes(root,ans,index);
+
+ 
+    
 }
 
 
@@ -297,7 +294,7 @@ void huffmancodes(char c[],int freq[], int size){
 
 int main(){
 
-char arr[] = { 'a', 'b', 'c', 'd', 'e', 'f' };
+char arr[] = { 'a', 'b', 'c', 'd', 'e', 'f'  };
     int freq[] = { 5, 9, 12, 13, 16, 45 };
  
     int size = sizeof(arr) / sizeof(arr[0]);
